4b54ed1815addabd9568e5c8a726280e6e554490
shilonyin <shilonyin@qq.com>
2026-09-25 01:44:28 +0800

    chore(l10n): 同步 app_localizations_en.dart 生成格式
    仅换行与转义形式变化，字符串内容不变。

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -1168,25 +1168,27 @@ class AppLocalizationsEn extends AppLocalizations {
   String get autoGroupDownloads => 'Auto-create series groups';
 
   @override
-  String get autoGroupDownloadsDescription => 'Put a new download into a group named after the video';
+  String get autoGroupDownloadsDescription =>
+      'Put a new download into a group named after the video';
 
   @override
   String get groupNameFromSeries => 'Name groups after the series';
 
   @override
-  String get groupNameFromSeriesUnavailable => 'No series name detected in this title';
+  String get groupNameFromSeriesUnavailable =>
+      'No series name detected in this title';
 
   @override
   String get groupNameTraditional => 'Use Traditional Chinese group names';
 
   @override
   String willUseExistingGroup(String name) {
-    return 'Will use existing group "$name"';
+    return 'Will use existing group \"$name\"';
   }
 
   @override
   String willCreateNewGroup(String name) {
-    return 'Will create new group "$name"';
+    return 'Will create new group \"$name\"';
   }
 
   @override
